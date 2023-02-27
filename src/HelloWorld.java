162b2a1c6ba68ca716a8d70ec9d258d18cf73e1e
GREATHOUSERC20 <greathouserc20@gcc.edu>
2023-02-27 11:24:47 -0500

    Updated program to ask user for name.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello World347");
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scn.next();
+        System.out.printf("Hello %s", name);
     }
 }
